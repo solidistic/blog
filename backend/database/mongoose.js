f0bdb34159d7d1488307cfc649fd5d4874746ad6
Janne Mulari <janne.mulari@gmail.com>
2021-10-16 21:32:59 +0300

    MongoDB uudelleen yhdistäminen, jos yhteys katkeaa

--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -2,18 +2,28 @@ const mongoose = require("mongoose");
 
 const url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/blog";
 
-mongoose.connect(url, {
+const connectionOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  useCreateIndex: true
-});
+  useCreateIndex: true,
+  server: {
+    auto_reconnect: true,
+  },
+};
+
+mongoose.connect(url, connectionOptions);
 
 const db = mongoose.connection;
 
-db.once("error", e => console.log("MongoDB error:", e));
+db.once("error", (e) => console.log("MongoDB error:", e));
 db.once("open", () => {
   console.log("Connected to MongoDB");
+});
+db.on("reconnect", () => console.log("MongoDB reconnected!"));
+db.on("disconnect", () => {
+  console.log("MongoDB disconnected.. retrying..");
+  mongoose.connect(url, connectionOptions);
 });
 
 module.exports = db;
