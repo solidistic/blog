6023c5d1a93353e83a108dde000936421311eb22
Janne Mulari <janne.mulari@gmail.com>
2021-10-18 00:09:04 +0300

    Lisää testejä

--- backend/server/middleware/auth.js
+++ backend/server/middleware/auth.js
@@ -1,22 +1,31 @@
 const User = require("../../database/models/user");
 
 module.exports = async (req, res, next) => {
+  console.log(req.signedCookies);
   try {
     if (!req.signedCookies.id && !req.signedCookies.jwt_token)
       throw new Error();
 
     const user = await User.findById(req.signedCookies.id);
+
+    console.log("user", user);
+
     if (!user) throw new Error();
 
     const tokenFound = user.tokens.find(
-      index => index.token === req.signedCookies.jwt_token
+      (index) => index.token === req.signedCookies.jwt_token
     );
+
+    console.log("token found", tokenFound);
 
     if (!tokenFound) throw new Error();
 
     const [validToken, message] = await user.validateToken(
       req.signedCookies.jwt_token
     );
+
+    console.log("valid", validToken);
+
     if (!validToken) throw new Error(message);
 
     req.user = user;
